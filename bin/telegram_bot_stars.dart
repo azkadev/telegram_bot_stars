6bf4ddfce127fb756bc9534bf25ca8d99026402e
￴￴ <>
2024-06-21 22:57:23 +0700

    up

---
+++ bin/telegram_bot_stars.dart
@@ -0,0 +1,5 @@
+import 'package:telegram_bot_stars/telegram_bot_stars.dart' as telegram_bot_stars;
+
+void main(List<String> arguments) {
+  print('Hello world: ${telegram_bot_stars.calculate()}!');
+}
---
+++ test/telegram_bot_stars_test.dart
@@ -0,0 +1,8 @@
+import 'package:telegram_bot_stars/telegram_bot_stars.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
